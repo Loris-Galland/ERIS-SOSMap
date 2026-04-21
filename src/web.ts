4f2bf93209bb1cb9f79713c4929bf29c7471d975
Loris Galland <loris.galland@etu.unice.fr>
2026-04-21 05:48:08 +0200

    feat(plugin): implement native offline emergency trigger for iOS

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,20 @@
 import { WebPlugin } from '@capacitor/core';
-
 import type { CapacitorErisSosmapPlugin } from './definitions';
 
 export class CapacitorErisSosmapWeb extends WebPlugin implements CapacitorErisSosmapPlugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
+  }
+
+  async triggerEmergency(options: { 
+    latitude: number; 
+    longitude: number; 
+    userId: string 
+  }): Promise<{ success: boolean; transmissionMethod: string }> {
+    
+    console.log('WEB MOCK - Emergency Triggered:', options);
+    
+    return { success: true, transmissionMethod: 'WEB_SIMULATION' };
   }
 }
